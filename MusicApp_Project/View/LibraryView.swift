c9b681ed4960acc819d01a172349e3fa6117e462
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-10-27 23:30:45 +0200

    Creating LibraryView.

---
+++ MusicApp_Project/View/LibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryView.swift
+//  MusicApp_Project
+//
+//  Created by Wizzard Sklyarov on 2021. 10. 27..
+//
+
+import SwiftUI
+
+struct LibraryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LibraryView_Previews: PreviewProvider {
+    static var previews: some View {
+        LibraryView()
+    }
+}
