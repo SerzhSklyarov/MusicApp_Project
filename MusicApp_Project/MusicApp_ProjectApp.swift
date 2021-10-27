54b13c1d6558dcc664f11939b922ddc8a55fe72e
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-10-27 21:48:59 +0200

    Initial Commit

---
+++ MusicApp_Project/MusicApp_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicApp_ProjectApp.swift
+//  MusicApp_Project
+//
+//  Created by Wizzard Sklyarov on 2021. 10. 27..
+//
+
+import SwiftUI
+
+@main
+struct MusicApp_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
