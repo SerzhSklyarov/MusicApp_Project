5efc7b990e3b9b6cb4b8ca75283f2732fcddd37d
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-10-30 01:47:49 +0200

    LibraryInfo_fixing.

--- MusicApp_Project/Library_Setup/LibraryInfo.swift
+++ MusicApp_Project/Library_Setup/LibraryInfo.swift
@@ -9,29 +9,21 @@ import SwiftUI
 
 struct LibraryInfo: View {
     var body: some View {
-        NavigationView{
-            
             VStack {
-                HStack{
                     Text("Ищете свою музыку?")
                         .font(.title2)
                         .fontWeight(.bold)
-                    
-                }
                 
                 Text("Здесь появится купленная Вами в iTunes Store музыка.")
-                    .foregroundColor(Color.gray)
                     .font(.title3)
-                multilineTextAlignment(.center)
+                    .multilineTextAlignment(.center)
                     .padding([.leading, .trailing], 28)
                     .foregroundColor(.secondary)
-                    .frame(width: 300)
-            }
+    }
             .navigationTitle("Медиатека")
             .padding()
             .frame(height: 600)
-        }
-    }
+}
 }
 
 struct LibraryInfo_Previews: PreviewProvider {
