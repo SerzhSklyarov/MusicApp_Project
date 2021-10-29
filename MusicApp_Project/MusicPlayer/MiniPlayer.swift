09667f081b1e9594a3e46814c72dd7d21fc0543b
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-10-30 01:49:22 +0200

    TabBar setup.

--- MusicApp_Project/MusicPlayer/MiniPlayer.swift
+++ MusicApp_Project/MusicPlayer/MiniPlayer.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct MiniPlayer: View {
+    @State var showDetails = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack {
+            Image("Picture0")
+                .resizable()
+                .frame(width: 50, height: 50)
+                .scaledToFit()
+            Text("Lady Gaga - Joanne")
+            Spacer()
+            
+            Button(action: {
+                self.showDetails.toggle()
+            }, label: {
+                Image(systemName: "play.fill")
+                    .resizable()
+                    .foregroundColor(.black)
+                    .frame(width: 18, height: 18)
+                    .scaledToFit()
+            })
+            
+            Spacer()
+                .frame(width: 13)
+            
+            Button(action: {
+                self.showDetails.toggle()
+            }, label: {
+                Image(systemName: "forward.fill")
+                    .resizable()
+                    .foregroundColor(.black)
+                    .frame(width: 25, height: 20)
+                    .scaledToFit()
+            })
+                .padding(5)
+        }
+        .padding()
+        .frame(height: 70)
+        .background(
+            VStack(spacing: 0) {
+                BlurView()
+                Divider()
+            })
+        .offset(y: -48)
     }
 }
 
